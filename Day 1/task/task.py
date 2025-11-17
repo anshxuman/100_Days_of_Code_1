80b45cf8bcb9d5f4bf31c43155edba820482100a
anshxuman <anshumangarg04@gmail.com>
2025-11-17 14:07:18 +0530

    day -01

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Hello Dear, How are you?")
+city = input("What city did you grew up in ? \n")
+pname = input("Give a name of a pet \n")
+print("Your pet name could be : " + city + " " + pname)
