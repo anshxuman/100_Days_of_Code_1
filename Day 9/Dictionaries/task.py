0444ddb58773bfaeb772605e6912729b0482422b
anshxuman <anshumangarg04@gmail.com>
2026-01-12 21:35:09 +0530

    DAY - 9

---
+++ Day 9/Dictionaries/task.py
@@ -0,0 +1,14 @@
+programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+print(programming_dictionary["Bug"])
+
+print(programming_dictionary["Function"])
+
+programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+empty_dictionary = {}
+
+programming_dictionary["Bug"] = "A moth in your computer."
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
