76696faaa9987e28ccc8c2923d7a545ea79e3a57
anshxuman <anshumangarg04@gmail.com>
2026-01-27 18:54:23 +0530

    DAY - 13

---
+++ Day 13/Describe the Problem/task.py
@@ -0,0 +1,20 @@
+def my_function():
+    for i in range(1, 20):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
+
+# Describe the Problem - Write your answers as comments:
+
+# 1. What is the for loop doing? -
+# it's trying to print "You got it" when the value of i becomes 20, which will never become
+# 20 bcause its starting from 1 and ending at 19
+
+
+# 2. When is the function meant to print "You got it"?
+# when i == 20
+
+# 3. What are your assumptions about the value of i?
+# i won't be 20, we need to make this loop run till 21
